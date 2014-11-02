e770d4c368a367802afc8168c1939bfee4778cbc
Lev <levangogohia@yandex.ru>
2014-11-02 16:38:18 +0300

    first commit

---
+++ parser/main.cpp
@@ -0,0 +1,107 @@
+#include <QCoreApplication>
+#include <QtCore>
+#include <QDebug>
+typedef struct {
+    QString password;
+    QString login;
+    QString site;
+} site;
+
+typedef struct system{
+    QString password;
+}systems;
+
+void parser(const QByteArray &text) {
+    QStringList systemWords;
+    systemWords << "mainpassword" << "login";
+    QStringList localWords;
+    localWords << "password" << "login" << "email";
+    QList<QByteArray> s = text.split('\n');
+    bool systemFlag = true;
+    bool systemCounter = false;
+    bool localFlag = false;
+    bool localCounter = false;
+    QMap<QString, QString> localMap;
+    QMap<QString, QString> systemMap;
+    QList< QMap<QString, QString> > systemList;
+    QList< QMap<QString, QString> > sitesList;
+    int i = 0;
+    while(i < s.size() - 1) {
+        QList<QByteArray> l;
+        l.append(s[i].split(':'));
+        QString key = QString(l.at(0));
+        QString value = QString(l.at(1));
+        if(systemFlag) {
+            if(!systemCounter) {
+                QString local = "";
+                foreach(local, systemWords) {
+                    if(local == key && key != QString("system")) systemMap[key] = value;
+                }
+                if(value == QString("end")) systemCounter = true;
+            }
+            if( !systemMap.isEmpty() && systemCounter) {
+                systemList.append(systemMap);
+                systemMap.clear();
+                systemCounter = false;
+            }
+        }
+        if (localFlag) {
+            if(!localCounter) {
+                QString local = "";
+                if(value == QString("start")) localMap["site"] = key;
+                foreach(local, localWords) {
+                    if(local == key && value != QString("end") && value!=QString("start")) {
+                        localMap[key] = value;
+                    }
+                }
+                if(value == QString("end")) localCounter = true;
+            }
+            if(!localMap.isEmpty() && localCounter) {
+                sitesList.append(localMap);
+                localMap.clear();
+                localCounter = false;
+            }
+        }
+        if(value == QString("end") && key == QString("system")) {
+            systemFlag = false;
+            localFlag = true;
+        }
+        i++;
+    }
+    /*i = 0;
+    while(i < sitesList.size()){
+            QMapIterator <QString, QString> Iter(sitesList[i]);
+            while(Iter.hasNext()) {
+                Iter.next();
+                qDebug() << "key: " << Iter.key() << "value: " << Iter.value();
+            }
+        i++;
+    }
+
+    i = 0;
+    qDebug() << systemList.size();
+    while(i < systemList.size()){
+            QMapIterator <QString, QString> Iter(systemList[i]);
+            while(Iter.hasNext()) {
+                Iter.next();
+                qDebug() << "key: " << Iter.key() << "value: " << Iter.value();
+            }
+        i++;
+    }*/
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QFile parseFile(qApp->applicationDirPath() + "/parser.txt");
+    if (!parseFile.open(QIODevice::ReadWrite)) {
+        qDebug() << "File can't open";
+        return a.exec();
+    }
+    QByteArray parseData = parseFile.readAll();
+    parseFile.close();
+    parser(parseData);
+    return a.exec();
+}
---
+++ parser/parser.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-10-31T09:28:12
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = parser
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
